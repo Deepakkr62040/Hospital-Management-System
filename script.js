c279df583052b298aa17b0a02f7191d64fee514b
Deepakkr62040 <deepakkr62040@gmail.com>
2025-06-24 01:33:59 +0530

    add login and signup page setup

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ const loginlink = document.querySelector('.login-link');
 registerLink.onclick = () => {
     wrapper.classList.add('active');
 }
+
+loginlink.onclick = () => {
+    wrapper.classList.remove('active');
+}
